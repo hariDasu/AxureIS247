bed5ceb391620da54681840826c17cc64f60fd5d
Srihari Rao <harirao3@gmail.com>
2013-10-27 17:20:43 -0400

    added some more pages

--- Login_files/axurerp_pagespecificscript.js
+++ Login_files/axurerp_pagespecificscript.js
@@ -4,4 +4,15 @@ $axure.eventManager.pageLoad(
 function (e) {
 
 });
-gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u23'] = 'center';
+gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u1'] = 'center';u4.tabIndex = 0;
+
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('oAuth.html');
+
+}
+});
+gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u23'] = 'center';
---
+++ oAuth_files/axurerp_pagespecificscript.js
@@ -0,0 +1,28 @@
+﻿for(var i = 0; i < 14; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u11'] = 'top';
+u12.style.cursor = 'pointer';
+$axure.eventManager.click('u12', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('loggedInHome.html');
+
+}
+});
+document.getElementById('u0_img').tabIndex = 0;
+
+u0.style.cursor = 'pointer';
+$axure.eventManager.click('u0', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('loggedInHome.html');
+
+}
+});
+gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u9'] = 'center';
