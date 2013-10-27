659d8b3560dcc400fa594152b5dfce8f6f874d65
Srihari Rao <harirao3@gmail.com>
2013-10-27 18:43:31 -0400

    added activities

--- data/sitemap.js
+++ data/sitemap.js
@@ -2,7 +2,7 @@
 
 (function() {
     var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,f,g,l),_(c,m,e,f,g,n),_(c,o,e,f,g,p)]);}; 
-var b="rootNodes",c="pageName",d="iPhone Frame for Desktop View",e="type",f="Wireframe",g="url",h="iPhone_Frame_for_Desktop_View.html",i="App Home",j="App_Home.html",k="Login",l="Login.html",m="oAuth",n="oAuth.html",o="loggedInHome",p="loggedInHome.html";
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,f,g,l),_(c,m,e,f,g,n),_(c,o,e,f,g,p),_(c,q,e,f,g,r)]);}; 
+var b="rootNodes",c="pageName",d="iPhone Frame for Desktop View",e="type",f="Wireframe",g="url",h="iPhone_Frame_for_Desktop_View.html",i="App Home",j="App_Home.html",k="Login",l="Login.html",m="oAuth",n="oAuth.html",o="loggedInHome",p="loggedInHome.html",q="createEvent",r="createEvent.html";
 return _creator();
 })();
