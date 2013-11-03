e8c0e1971d1e83494875dc185513abd0f561bd15
Srihari Rao <harirao3@gmail.com>
2013-11-03 18:10:36 -0500

    added more 3.0 pages

--- loggedInHome_files/axurerp_pagespecificscript.js
+++ loggedInHome_files/axurerp_pagespecificscript.js
@@ -1,21 +1,34 @@
-﻿for(var i = 0; i < 21; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+﻿for(var i = 0; i < 52; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
 });
-gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u16'] = 'center';document.getElementById('u15_img').tabIndex = 0;
+gv_vAlignTable['u50'] = 'top';gv_vAlignTable['u51'] = 'top';gv_vAlignTable['u20'] = 'top';document.getElementById('u21_img').tabIndex = 0;
 
-u15.style.cursor = 'pointer';
-$axure.eventManager.click('u15', function(e) {
+u21.style.cursor = 'pointer';
+$axure.eventManager.click('u21', function(e) {
 
 if (true) {
 
-	self.location.href=$axure.globalVariableProvider.getLinkUrl('searchResults.html');
+	SetPanelVisibility('u27','','none',500);
 
 }
 });
-gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u4'] = 'top';document.getElementById('u5_img').tabIndex = 0;
+gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u33'] = 'center';document.getElementById('u34_img').tabIndex = 0;
+
+u34.style.cursor = 'pointer';
+$axure.eventManager.click('u34', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u27','hidden','none',500);
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+});
+gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u4'] = 'top';document.getElementById('u5_img').tabIndex = 0;
 
 u5.style.cursor = 'pointer';
 $axure.eventManager.click('u5', function(e) {
@@ -37,4 +50,15 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u8'] = 'center';
+gv_vAlignTable['u8'] = 'center';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u49'] = 'top';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u14'] = 'center';document.getElementById('u15_img').tabIndex = 0;
+
+u15.style.cursor = 'pointer';
+$axure.eventManager.click('u15', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('searchResults.html');
+
+}
+});
+gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u19'] = 'top';
